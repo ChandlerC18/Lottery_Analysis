4d7285cc232e6568dd03e84e0d6a3697a11f8282
ChandlerC18 <chandlercheung@Chandlers-MacBook-Pro.local>
2022-07-29 15:54:33 -0400

    fix index saving

--- get_data.py
+++ get_data.py
@@ -8,7 +8,7 @@ df = pd.DataFrame(columns=['Date', 'Ball 1', 'Ball 2', 'Ball 3', 'Ball 4', 'Ball
 
 for i in range(1996, 2023):
     print(i) # print year
-    
+
     # Making a GET request
     # r = requests.get('https://www.megamillions.com/Winning-Numbers/Previous-Drawings.aspx')
     r = requests.get(f'https://www.nylottery.org/mega-millions/past-winning-numbers/{i}')
@@ -24,8 +24,8 @@ for i in range(1996, 2023):
     for j in range(len(content) - 1, -1, -3):
         date = content[j - 2].text.split('\n')
         numbers = content[j - 1].text.split('\n')
-        megaplier = numbers[7].split(' ')[1] if numbers[7] != '' else ''
+        megaplier = f"x{numbers[7].split(' ')[1][1:]}" if numbers[7] != '' else ''
         df.loc[len(df.index)] = [date[1], numbers[1], numbers[2], numbers[3], numbers[4], numbers[5], numbers[6], megaplier]
 
-df.to_csv('lottery_data.csv')
+df.to_csv('lottery_data.csv', index=False)
 print(df)
